5c23f2c108f024fbdff54e3b0fa973984d5fc8d2
khoatiendao <88887127+khoatiendao@users.noreply.github.com>
2024-04-28 14:41:20 +0700

    update

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ document.getElementById("showButton").addEventListener("click", function() {
 // text typing 
 var textmessage_1 = document.getElementById("text-message_1")
 var textmessage_2 = document.getElementById("text-message_2")
-var message_string = "Được làm trọn bằng cả tấm lòng của Khoa";
-var message_string_2 = "Đặc biệt gửi đến Nhi Cute";
+var message_string = "Được làm trọn bằng cả tấm lòng của ...";
+var message_string_2 = "Đặc biệt gửi đến ...";
 
 let messLength = 0;
 let messIndex = 0;
